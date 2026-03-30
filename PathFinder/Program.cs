d23144e005d0c5d33dd5a287c1ded8e4ccb7e249
Mykola Domanskyi <mykola.domanskyi.25@kse.org.ua>
2026-03-30 18:49:41 +0300

    Add total time calculation

--- PathFinder/Program.cs
+++ PathFinder/Program.cs
@@ -5,7 +5,7 @@ var optionsToGenerate = new MapGeneratorOptions()
 {
     Height = 10,
     Width = 100,
-    Noise = 0.3f,
+    Noise = 0.5f,
     AddTraffic = true,
     TrafficSeed = 1234
 };
@@ -21,6 +21,29 @@ var (path, nodesVisited) = bfs.FindPath(map!, start, destination);
 
 
 new MapPrinter().Print(map, path);
+
+double totaltime = 0;
+foreach (var p in path)
+{
+    if (p.Equals(start)) continue;
+    string cell = map[p.Column, p.Row];
+
+    int n;
+    if (cell.Equals(" "))
+    {
+        n = 1;
+    }
+    else
+    {
+        n = int.Parse(cell);
+    }
+
+    double v = 60.0 - (n - 1.0) * 6.0;
+    double t = 1.0 / v;
+    totaltime = totaltime + t;
+}
+
+Console.WriteLine($"Total time: {totaltime:F2}");
 
 Console.WriteLine($"\nNodes Visited: {nodesVisited}");
 Console.WriteLine($"Path length: {path.Count}");
